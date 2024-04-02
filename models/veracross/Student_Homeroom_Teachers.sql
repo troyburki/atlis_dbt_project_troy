02cd9c0b32c240f17e2f72cc46510202b70522b2
hudson-harpertech <102432543+hudson-harpertech@users.noreply.github.com>
2024-04-02 14:39:44 -0700

    first commit

---
+++ models/veracross/Student_Homeroom_Teachers.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+SELECT
+    id as student_id,
+    last_name,
+    first_name,
+    json_extract(homeroom_teacher, '$.name') as homeroom_teacher_name,
+FROM `atlis-data-workshop.Veracross_AB.Get_Students`
