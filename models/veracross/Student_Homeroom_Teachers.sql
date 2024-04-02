a75b177b47851bec14f1b14fd0074d5beac7d8c6
hudson-harpertech <102432543+hudson-harpertech@users.noreply.github.com>
2024-04-02 15:04:26 -0700

    Fix JSON_EXTRACT function in Student_Homeroom_Teachers.sql

--- models/veracross/Student_Homeroom_Teachers.sql
+++ models/veracross/Student_Homeroom_Teachers.sql
@@ -4,5 +4,5 @@ SELECT
     id as student_id,
     last_name,
     first_name,
-    REPLACE(JSON_EXTRACT(homeroom_teacher, '$.name'),'"', '') as homeroom_teacher_name,
+    json_extract(homeroom_teacher, '$.name') as homeroom_teacher_name,
 FROM `atlis-data-workshop.Veracross_AB.Get_Students`
